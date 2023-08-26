cb71bd6fd579f45f42f0807863998dc519673a33
Newton Ombese <newtonombese1@gmail.com>
2023-08-26 12:44:24 +0300

    Add a store for my redux

--- src/store.js
+++ src/store.js
@@ -1,5 +1,9 @@
-import { ConfigureStore } from "@reduxjs/toolkit";
-
-export const store = ConfigureStore({
-  reducer: {},
-})
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/cartSlice';
+import modalReducer from './features/modal/modalSlice';
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    modal: modalReducer,
+  },
+});
